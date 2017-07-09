4b2c316ae4cc1a24bf488704b9437e59bd48a1c5
Isaac Sloan <isaac@isaacsloan.com>
2017-07-08 19:10:23 -0600

    specs work

--- spec/str_compressor_spec.cr
+++ spec/str_compressor_spec.cr
@@ -1,9 +1,23 @@
 require "./spec_helper"
 
 describe StrCompressor do
-  # TODO: Write tests
+  context Gzip do
+    gzip_str = "hello gzip compressed world"
 
-  it "works" do
-    false.should eq(true)
+    it "compresses and uncompresses strings with gzip" do
+      Gzip.read(Gzip.write(gzip_str)).should eq gzip_str
+    end
+  end
+
+  context Flate do
+    flate_str = "hello flate compressed world"
+
+    it "compresses strings with flate" do
+      Flate.write(flate_str).should eq "\xCBH\xCD\xC9\xC9WH\xCBI,IUH\xCE\xCF-(J-.NMQ(\xCF/\xCAI\u0001\u0000"
+    end
+
+    it "uncompresses strings with flate" do
+      Flate.read("\xCBH\xCD\xC9\xC9WH\xCBI,IUH\xCE\xCF-(J-.NMQ(\xCF/\xCAI\u0001\u0000").should eq flate_str
+    end
   end
 end
